aa96d3cb5357e649b2e20d0316acc48c9461f278
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-21 00:32:34 +0530

    Datatypes file updated

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,16 +2,13 @@
 
 // alert(3+3); // we are using nodejs, not browser
 
-console.log(3 
-    +3) // code readability should be high
+// console.log(3 
+//     +3) // code readability should be high
 
-console.log("Sanket")
+// console.log("Sanket")
 
 
-let name = "Sanket";
-let age = 20;
-let isLoggedIn = false
-let state;
+// Primitive datatypes
 
 // number => 2 to power 53
 // bigint
@@ -21,8 +18,34 @@ let state;
 // undefined => 
 // symbol => unique
 
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
 
-// object
+let name = "Sanket";
+let age = 20;
+let temp = null;
+let isLoggedIn = false
+let state;
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId);
+
+let bigNumber = 1694961651349641984651984n;
+
+// Reference (Non Primitive)
+// Array, Object, Functions
+
+const temp1 = ["abc", "pqr", "xyz"];
+
+const myObj = {
+    name : "Sanket",
+    age : 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
