9770e42f71420d02300307d842792a7f121a78e8
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-20 23:56:56 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,5 +30,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let a = 33;
 let str = String(a);
-console.log(str);
-console.log(typeof str);
+// console.log(str);
+// console.log(typeof str);
+
+// *********OPERATIONS**********
+
+let value = 4;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " World";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4)/2*(5+2));
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+// gameCounter++;
+console.log(gameCounter);
