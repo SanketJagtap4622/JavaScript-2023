72eee22ad47475008379085e64b480c33755a792
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-20 23:32:14 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = 33;
+// let score = "33";
+let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = "Sanket";
+// let score = true;
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 
+// 0 => false
+// "" => false 
+// "Sanket" => true 
+
+let a = 33;
+let str = String(a);
+console.log(str);
+console.log(typeof str);
