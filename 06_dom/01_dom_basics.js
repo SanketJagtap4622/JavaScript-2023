6b1d805b0d0334f6f3895dbb361f197240575e74
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-10-04 17:14:40 +0530

    DOM basics

---
+++ 06_dom/01_dom_basics.js
@@ -0,0 +1,21 @@
+console.log(window);
+
+console.log(window.document);
+
+console.log(document);
+
+console.dir(document);
+
+console.log(document.baseURI)
+
+console.log(document.location)
+
+console.log(document.links)
+
+console.log(document.getElementById("firstHeading"))
+
+document.getElementById("firstHeading")
+
+document.getElementById("firstHeading").innerHTML = "Chai aur Code"
+
+document.getElementById("firstHeading").innerHTML = "<h1>Chai aur Code</h1>"
